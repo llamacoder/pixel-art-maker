75cbbf3451c38df3a34969420c7feacffc22191c
llamacoder <30612433+llamacoder@users.noreply.github.com>
2017-10-13 11:34:12 -0600

    drawing grid and first row of paint chips

--- pixel.js
+++ pixel.js
@@ -1,19 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
   function makeDiv() {
     let div = document.createElement('div');
-    div.className = 'gridSquare';
-    div.style.backgroundColor = "#ffffff";
+    div.className = 'pixelSquare';
     return div;
   }
 
   function addDivs(num) {
     let grid = document.querySelector('.grid');
-    // let rowDiv = document.
     for (let i = 0; i < num; i++) {
       grid.appendChild(makeDiv());
     }
   }
 
-  addDivs(64);
-  addDivs(64);
+  addDivs(28*55);
 });
